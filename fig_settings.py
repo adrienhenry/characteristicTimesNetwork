8c1c1dfa010d0ea9b80faabba761eea0325a5f3a
Adrien Henry <adrien.ym.henry@gmail.com>
2016-05-19 14:49:16 -0400

    timeVsSaturations

--- fig_settings.py
+++ fig_settings.py
@@ -1,11 +1,11 @@
 ## Figure size:
-fig_width_pt = 246.0  # Get this from LaTeX using \showthe\columnwidth
-inches_per_pt = 1.0/72.27               # Convert pt to inches
-golden_mean = (np.sqrt(5)-1.0)/2.0         # Aesthetic ratio
-fig_width = fig_width_pt*inches_per_pt  # width in inches
-fig_height =fig_width*golden_mean       # height in inches
-fig_size = [fig_width,fig_height]
-rc("figure",figsize = fig_size)
+# fig_width_pt = 246.0  # Get this from LaTeX using \showthe\columnwidth
+# inches_per_pt = 1.0/72.27               # Convert pt to inches
+# golden_mean = (np.sqrt(5)-1.0)/2.0         # Aesthetic ratio
+# fig_width = fig_width_pt*inches_per_pt  # width in inches
+# fig_height =fig_width*golden_mean       # height in inches
+# fig_size = [fig_width,fig_height]
+# rc("figure",figsize = fig_size)
 text_color = "#000000"#"#424242"
 ms_size = 6
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
